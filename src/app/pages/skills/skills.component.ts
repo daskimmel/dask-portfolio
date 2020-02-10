6bc21ccd7f02da64259974f73fdfa2424318b40e
DaskJeeves <daskjeeves@gmail.com>
2020-02-09 21:00:23 -0700

    Added more skill logos + much styling to make them look better

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -9,8 +9,20 @@ import {TileAttributes} from '../../pojo/tile-attributes';
 export class SkillsComponent implements OnInit {
 
     tileList: Array<TileAttributes> = [
-        {'name': 'angular', 'image': 'assets/images/angular_logo.png'},
-        {'name': 'spring', 'image': 'assets/images/spring_logo.png'}
+        {'name': 'ANGULAR', 'image': 'assets/images/angular_logo.png'},
+        {'name': 'SPRING', 'image': 'assets/images/spring_logo.png'},
+        {'name': 'DJANGO', 'image': 'assets/images/django_logo.png'},
+        {'name': 'REACT', 'image': 'assets/images/react_logo.png'},
+        {'name': 'HTML5', 'image': 'assets/images/html5_logo.png'},
+        {'name': 'CSS3', 'image': 'assets/images/css3_logo.png'},
+        {'name': 'JAVASCRIPT', 'image': 'assets/images/javascript_logo.png'},
+        {'name': 'TYPESCRIPT', 'image': 'assets/images/typescript_logo.png'},
+        {'name': 'JAVA', 'image': 'assets/images/java_logo.png'},
+        {'name': 'PYTHON', 'image': 'assets/images/python_logo.png'},
+        {'name': 'DOCKER', 'image': 'assets/images/docker_logo.png'},
+        {'name': 'GIT', 'image': 'assets/images/git_logo.png'},
+        {'name': 'POSTGRES', 'image': 'assets/images/postgres_logo.png'},
+        {'name': 'REDUX', 'image': 'assets/images/redux_logo.png'},
     ];
 
     constructor() {
