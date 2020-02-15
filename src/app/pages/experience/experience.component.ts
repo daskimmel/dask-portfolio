57cb896fd61e0c3b32cdfb4f34aef124041e5982
DaskJeeves <daskjeeves@gmail.com>
2020-02-15 12:32:25 -0700

    Got element fade in on scroll working on experience page"

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,32 +1,33 @@
 import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'experience-component',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+    selector: 'experience-component',
+    templateUrl: './experience.component.html',
+    styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor(private elem: ElementRef) { }
+    constructor(private elem: ElementRef) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  @HostListener("window:scroll", [])
-  checkForHiddenElements(): void {
-    // do some stuff here when the window is scrolled
-    const verticalOffset = window.pageYOffset
-        || document.documentElement.scrollTop
-        || document.body.scrollTop || 0;
-    console.log("pageYOffset", window.pageYOffset);
-    console.log("document.documentElement.scrollTop", document.documentElement.scrollTop);
-    console.log("document.body.scrollTop", document.body.scrollTop);
+    @HostListener('window:scroll', [])
+    checkForHiddenElements(): void {
+        // do some stuff here when the window is scrolled
+        const verticalOffset = window.pageYOffset
+            || document.documentElement.scrollTop
+            || document.body.scrollTop || 0;
+        const bottomOfWindow = document.documentElement.clientHeight + verticalOffset;
 
-    let elements: NodeListOf<Element> = this.elem.nativeElement.querySelectorAll('.hideme');
+        let elements: NodeListOf<Element> = this.elem.nativeElement.querySelectorAll('.hidden');
 
-    elements.forEach(() => {
+        elements.forEach((el: HTMLElement) => {
+            if(bottomOfWindow > el.offsetTop + el.clientHeight){
+                el.className = "fadeIn";
+            }
+        });
 
-    })
-
-  }
+    }
 }
